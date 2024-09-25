9bc9cd9f7151f52cbbde57c416f53cf69add9e6e
udiladido <89720389+udiladido@users.noreply.github.com>
2024-09-25 17:03:29 +0900

    chore
    .sln 파일 생성

--- obj/Debug/net8.0/TextGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/TextGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d776d775583a90dddeb16ce9a635013d9b9e366")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63f5fbc3d71911737f593e7495b38b3e33de167a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
